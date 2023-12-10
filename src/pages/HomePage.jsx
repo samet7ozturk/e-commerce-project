461f429862425e3a33be18a31c22a916b45514fa
samet7ozturk <sametozturk973@gmail.com>
2023-12-10 18:58:22 +0300

    BestSellerProduct import

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import Header from "../layout/Header";
 import { CarouselTransition } from "../layout/Carousel";
 import EditorsPick from "../layout/EditorsPick";
+import BestSellerProduct from "../layout/BestSellerProducts";
 
 const HomePage = () => {
   return (
@@ -8,6 +9,7 @@ const HomePage = () => {
       <Header />
       <CarouselTransition />
       <EditorsPick />
+      <BestSellerProduct />
     </main>
   );
 };
