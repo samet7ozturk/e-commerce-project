85b87a2d11d6a229767724623317dbafdee4861b
samet7ozturk <sametozturk973@gmail.com>
2023-12-10 18:09:16 +0300

    Montserrat font import

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,11 @@ module.exports = withMT({
     "./node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        montserrat: ["Montserrat"],
+      },
+    },
   },
   plugins: [],
 });
