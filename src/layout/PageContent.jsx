adfa6caa415ffafd9e9ea7fdac22cf05204aeb8f
samet7ozturk <sametozturk973@gmail.com>
2023-12-11 19:32:23 +0300

    Add product page route

--- src/layout/PageContent.jsx
+++ src/layout/PageContent.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 
 import HomePage from "../pages/HomePage";
+import ProductPage from "../pages/ProductPage";
 
 const PageContent = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<HomePage />} />
+
+        <Route path="/product-page" element={<ProductPage />} />
 
         <Route path="*" element={<h1>404 Not Found Page</h1>} />
       </Routes>
