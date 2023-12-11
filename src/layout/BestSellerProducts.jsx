3813afafaadec1b7ddc22546a3527a71e6747bf4
samet7ozturk <sametozturk973@gmail.com>
2023-12-11 16:24:58 +0300

    Add px[...]

--- src/layout/BestSellerProducts.jsx
+++ src/layout/BestSellerProducts.jsx
@@ -11,7 +11,7 @@ import img8 from "../assets/bestseller-products-8.png";
 
 const BestSellerProduct = () => {
   return (
-    <main className="flex flex-col h-[1652px]">
+    <main className="flex flex-col px-[10%]">
       <div className="flex flex-col items-center pt-24 pb-6">
         <p className="text-[#737373] text-xl pb-2">Featured Products</p>
         <h2 className="text-[#252B42] text-2xl font-bold pb-2">
@@ -21,37 +21,69 @@ const BestSellerProduct = () => {
           Problems trying to resolve the conflict between
         </p>
       </div>
-      <div className="flex flex-wrap justify-center gap-x-8 gap-y-16 pt-14">
+      <div className="flex flex-wrap justify-center gap-x-8 gap-y-16 pt-14 pb-20">
         <div>
-          <img src={img1} alt="img1" className="" />
+          <img
+            src={img1}
+            alt="img1"
+            className="cursor-pointer hover:scale-110 hover:drop-shadow-lg transition duration-300"
+          />
           <ProductCard />
         </div>
         <div>
-          <img src={img2} alt="img2" />
+          <img
+            src={img2}
+            alt="img2"
+            className="cursor-pointer hover:scale-110 hover:drop-shadow-lg transition duration-300"
+          />
           <ProductCard />
         </div>
         <div>
-          <img src={img3} alt="img3" />
+          <img
+            src={img3}
+            alt="img3"
+            className="cursor-pointer hover:scale-110 hover:drop-shadow-lg transition duration-300"
+          />
           <ProductCard />
         </div>
         <div>
-          <img src={img4} alt="img4" />
+          <img
+            src={img4}
+            alt="img4"
+            className="cursor-pointer hover:scale-110 hover:drop-shadow-lg transition duration-300"
+          />
           <ProductCard />
         </div>
         <div>
-          <img src={img5} alt="img5" />
+          <img
+            src={img5}
+            alt="img5"
+            className="cursor-pointer hover:scale-110 hover:drop-shadow-lg transition duration-300"
+          />
           <ProductCard />
         </div>
         <div>
-          <img src={img6} alt="img6" />
+          <img
+            src={img6}
+            alt="img6"
+            className="cursor-pointer hover:scale-110 hover:drop-shadow-lg transition duration-300"
+          />
           <ProductCard />
         </div>
         <div>
-          <img src={img7} alt="img7" />
+          <img
+            src={img7}
+            alt="img7"
+            className="cursor-pointer hover:scale-110 hover:drop-shadow-lg transition duration-300"
+          />
           <ProductCard />
         </div>
         <div>
-          <img src={img8} alt="img8" />
+          <img
+            src={img8}
+            alt="img8"
+            className="cursor-pointer hover:scale-110 hover:drop-shadow-lg transition duration-300"
+          />
           <ProductCard />
         </div>
       </div>
