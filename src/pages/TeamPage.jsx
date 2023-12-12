105d2ee816c9b38814c6dedbb21aaf86946ca14c
samet7ozturk <sametozturk973@gmail.com>
2023-12-12 12:21:16 +0300

    Team page template

---
+++ src/pages/TeamPage.jsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+const TeamPage = () => {
+  return (
+    <main className=" font-montserrat">
+      <Header />
+      <Footer />
+    </main>
+  );
+};
+
+export default TeamPage;
