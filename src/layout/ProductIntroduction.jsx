80920681f111ddc4011ac539ef7c395a8ae62614
samet7ozturk <sametozturk973@gmail.com>
2023-12-14 22:48:01 +0300

    Responsive complete

--- src/layout/ProductIntroduction.jsx
+++ src/layout/ProductIntroduction.jsx
@@ -4,12 +4,12 @@ import svg2 from "../assets/readmore-button.svg";
 
 const ProductIntroduction = () => {
   return (
-    <main className="flex justify-center">
+    <main className="flex justify-center flex-wrap px-[15%] xl:px-0 flex-col-reverse xl:flex-row">
       <div>
-        <img src={img} alt="img" className="" />
+        <img src={img} alt="img" className="pt-8 xl:pt-0" />
       </div>
-      <div className="flex flex-col justify-center">
-        <div className="flex flex-col items-center">
+      <div className="flex flex-col justify-center text-center">
+        <div className="flex flex-col items-center xl:items-start">
           <p className="text-[#BDBDBD] font-bold pb-8">SUMMER 2020</p>
           <h2 className="text-[#252B42] text-[40px] font-bold pb-8">
             Part of the Neural Universe
@@ -19,7 +19,7 @@ const ProductIntroduction = () => {
           </p>
         </div>
         <div className="flex flex-wrap justify-center">
-          <div className="flex gap-2">
+          <div className="flex gap-6 xl:gap-2 flex-wrap xl:flex-nowrap justify-center">
             <img
               src={svg1}
               alt="svg1"
