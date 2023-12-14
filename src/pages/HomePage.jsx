dec0fdf1cbd5845aedca32fc374a8264a4c2bf8b
samet7ozturk <sametozturk973@gmail.com>
2023-12-15 01:21:44 +0300

    Modified

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -13,6 +13,15 @@ const HomePage = () => {
       <Header />
       <Carousel1 />
       <EditorsPick />
+      <div className="flex flex-col items-center pt-24 pb-6">
+        <p className="text-[#737373] text-xl pb-2">Featured Products</p>
+        <h2 className="text-[#252B42] text-2xl font-bold pb-2">
+          BESTSELLER PRODUCTS
+        </h2>
+        <p className="text-[#737373] text-sm">
+          Problems trying to resolve the conflict between
+        </p>
+      </div>
       <BestSellerProduct />
       <Carousel2 />
       <ProductIntroduction />
