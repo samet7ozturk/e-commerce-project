479ce30d72b2cbb249d368e41c6338258a513677
samet7ozturk <sametozturk973@gmail.com>
2023-12-15 13:49:58 +0300

    Team page route add

--- src/components/TeamCard.jsx
+++ src/components/TeamCard.jsx
@@ -7,14 +7,17 @@ import {
 import img1 from "../assets/team-1-user-1.jpg";
 import img2 from "../assets/team-1-user-2.jpg";
 import img3 from "../assets/team-1-user-3.jpg";
+import { Link } from "react-router-dom";
 
 const TeamCard = () => {
   return (
     <main className="flex items-center gap-8 pt-8 xl:pt-28 flex-col xl:flex-row">
       <div className="hover:scale-110 transition duration-300">
-        <div>
-          <img src={img1} alt="img1" className="cursor-pointer" />
-        </div>
+        <Link to="/team-page">
+          <div>
+            <img src={img1} alt="img1" className="cursor-pointer" />
+          </div>
+        </Link>
         <div className="flex flex-col items-center">
           <p className="text-[#252B42] font-bold pt-8">Username</p>
           <p className="text-[#737373] text-sm font-bold pt-4">Profession</p>
@@ -44,9 +47,11 @@ const TeamCard = () => {
         </div>
       </div>
       <div className="hover:scale-110 transition duration-300">
-        <div>
-          <img src={img2} alt="img2" className="cursor-pointer" />
-        </div>
+        <Link to="/team-page">
+          <div>
+            <img src={img2} alt="img2" className="cursor-pointer" />
+          </div>
+        </Link>
         <div className="flex flex-col items-center">
           <p className="text-[#252B42] font-bold pt-8">Username</p>
           <p className="text-[#737373] text-sm font-bold pt-4">Profession</p>
@@ -76,9 +81,11 @@ const TeamCard = () => {
         </div>
       </div>
       <div className="hover:scale-110 transition duration-300">
-        <div>
-          <img src={img3} alt="img3" className="cursor-pointer" />
-        </div>
+        <Link to="/team-page">
+          <div>
+            <img src={img3} alt="img3" className="cursor-pointer" />
+          </div>
+        </Link>
         <div className="flex flex-col items-center">
           <p className="text-[#252B42] font-bold pt-8">Username</p>
           <p className="text-[#737373] text-sm font-bold pt-4">Profession</p>
