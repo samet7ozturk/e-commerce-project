04e71ec1f2979a2ac1297d42c8ef9ee5a5428cc9
samet7ozturk <sametozturk973@gmail.com>
2023-12-18 12:14:57 +0300

    Create global actions template

---
+++ src/store/actions/globalActions.js
@@ -0,0 +1,16 @@
+export const setRoles = (roles) => ({
+  type: "SET_ROLES",
+  payload: roles,
+});
+export const setCategories = (categories) => ({
+  type: "SET_CATEGORİES",
+  payload: categories,
+});
+export const setTheme = (theme) => ({
+  type: "SET_THEME",
+  payload: theme,
+});
+export const setLanguage = (language) => ({
+  type: "SET_LANGUAGE",
+  payload: language,
+});
