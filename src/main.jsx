4a5f18d17d56cd8dae73ec26cfaeef5d8ce4a7c1
samet7ozturk <sametozturk973@gmail.com>
2023-12-19 00:02:58 +0300

    Store import

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
+import store from "./store/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
