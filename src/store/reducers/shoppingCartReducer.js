ee578b04e139f843e832e2a10b433dfbd2077839
samet7ozturk <sametozturk973@gmail.com>
2023-12-18 11:30:39 +0300

    Create shopping cart reducer

---
+++ src/store/reducers/shoppingCartReducer.js
@@ -0,0 +1,14 @@
+const initialState = {
+  cart: [],
+  payment: {},
+  address: {},
+};
+
+const shoppingCartReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default shoppingCartReducer;
