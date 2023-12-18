b0787081e3bccfaf4ea0cf45a6d2aeca033975a7
samet7ozturk <sametozturk973@gmail.com>
2023-12-18 11:59:16 +0300

    Create store reducer

---
+++ src/store/reducers/storeReducer.js
@@ -0,0 +1,10 @@
+const initialState = {};
+
+const storeReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default storeReducer;
