99a72f27b8b3855248a9cddf537a16e4ca62e341
samet7ozturk <sametozturk973@gmail.com>
2023-12-19 12:30:41 +0300

    Cases added

--- src/store/reducers/userReducer.js
+++ src/store/reducers/userReducer.js
@@ -1,7 +1,11 @@
-const initialState = {};
+const initialState = { id: null, name: "", email: "" };
 
 const userReducer = (state = initialState, action) => {
   switch (action.type) {
+    case "LOGIN_USER_SUCCESS":
+      return action.payload;
+    case "LOGIN_USER_FAILURE":
+      return action.payload;
     default:
       return state;
   }
