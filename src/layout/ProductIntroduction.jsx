3b31783d23a17ebfaf42612b75245a25b9ea2402
samet7ozturk <sametozturk973@gmail.com>
2023-12-21 14:31:43 +0300

    Responsive complete

--- src/layout/ProductIntroduction.jsx
+++ src/layout/ProductIntroduction.jsx
@@ -1,14 +1,16 @@
-import img from "../assets/product-introduction.png";
+import img1 from "../assets/product-introduction-1.png";
+import img2 from "../assets/product-introduction-2.png";
 import svg1 from "../assets/buynow-button.svg";
 import svg2 from "../assets/readmore-button.svg";
 
 const ProductIntroduction = () => {
   return (
-    <main className="flex justify-center flex-wrap px-[15%] xl:px-0 flex-col-reverse xl:flex-row">
+    <main className="flex justify-center flex-wrap px-0 flex-col-reverse xl:flex-row">
       <div>
-        <img src={img} alt="img" className="pt-8 xl:pt-0" />
+        <img src={img1} alt="img1" className="pt-8 xl:pt-0 hidden xl:block" />
+        <img src={img2} alt="img2" className="pt-8 xl:pt-0 block xl:hidden" />
       </div>
-      <div className="flex flex-col justify-center text-center">
+      <div className="flex flex-col justify-center text-center xl:px-0 px-[15%] xl:pt-0 pt-28">
         <div className="flex flex-col items-center xl:items-start">
           <p className="text-[#BDBDBD] font-bold pb-8">SUMMER 2020</p>
           <h2 className="text-[#252B42] text-[40px] font-bold pb-8">
