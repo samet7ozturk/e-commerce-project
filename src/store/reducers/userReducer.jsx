f8f1e52156a84ef1696c2c9f09b9ae64c908578f
samet7ozturk <sametozturk973@gmail.com>
2023-12-27 12:29:07 +0300

    Success case complete

--- src/store/reducers/userReducer.jsx
+++ src/store/reducers/userReducer.jsx
@@ -3,7 +3,12 @@ const initialState = { id: null, name: "", email: "" };
 const userReducer = (state = initialState, action) => {
   switch (action.type) {
     case "LOGIN_USER_SUCCESS":
-      return action.payload;
+      return {
+        ...state,
+        id: action.payload.role_id,
+        name: action.payload.name,
+        email: action.payload.email,
+      };
     case "LOGIN_USER_FAILURE":
       return action.payload;
     default:
