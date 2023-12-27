3329986c953c7e2e477c970c39954992381698d0
samet7ozturk <sametozturk973@gmail.com>
2023-12-28 00:09:37 +0300

    Added categories dropdown menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { loginExit } from "../store/actions/userActions";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -15,8 +17,34 @@ import {
   faTwitter,
 } from "@fortawesome/free-brands-svg-icons";
 import { faUser, faHeart } from "@fortawesome/free-regular-svg-icons";
+import {
+  Menu,
+  MenuHandler,
+  MenuItem,
+  MenuList,
+} from "@material-tailwind/react";
 
 const Header = () => {
+  const [openMenu1, setOpenMenu1] = useState(false);
+  const [openMenu2, setOpenMenu2] = useState(false);
+  const categories = useSelector((store) => store.global.categories);
+  const femaleCategories = categories.filter(
+    (category) => category.gender === "k"
+  );
+  const maleCategories = categories.filter(
+    (category) => category.gender === "e"
+  );
+
+  const user = useSelector((state) => state.user);
+  const token = localStorage.getItem("token");
+  console.log(user);
+  const dispatch = useDispatch();
+
+  const tokenDelete = () => {
+    localStorage.removeItem("token");
+    dispatch(loginExit());
+  };
+
   return (
     <main className="font-bold">
       <header className="h-[58px] px-[3%] hidden flex-wrap justify-between xl:flex bg-[#252B42] text-white">
@@ -92,17 +120,98 @@ const Header = () => {
             <div className="flex items-center">
               <Link to="/pages">Pages</Link>
             </div>
+            <div className="flex items-center">
+              <Menu placement="bottom-start">
+                <MenuHandler>
+                  <button>Categories</button>
+                </MenuHandler>
+                <MenuList>
+                  <Menu
+                    placement="right-start"
+                    open={openMenu1}
+                    handler={setOpenMenu1}
+                    allowHover
+                    offset={15}
+                  >
+                    <MenuHandler className="flex items-center justify-between">
+                      <MenuItem>
+                        <p className="text-[#737373] text-3xl xl:text-sm font-normal xl:font-bold font-montserrat">
+                          Kadın
+                        </p>
+                        <faPhone
+                          strokeWidth={2.5}
+                          className={`h-3.5 w-3.5 transition-transform ${
+                            openMenu1 ? "rotate-90" : ""
+                          }`}
+                        />
+                      </MenuItem>
+                    </MenuHandler>
+                    <MenuList>
+                      {femaleCategories.map((category) => (
+                        <MenuItem
+                          className="text-[#737373] text-3xl xl:text-sm font-normal xl:font-bold font-montserrat"
+                          key={category.id}
+                        >
+                          {category.title}
+                        </MenuItem>
+                      ))}
+                    </MenuList>
+                  </Menu>
+                  <Menu
+                    placement="right-start"
+                    open={openMenu2}
+                    handler={setOpenMenu2}
+                    allowHover
+                    offset={15}
+                  >
+                    <MenuHandler className="flex items-center justify-between">
+                      <MenuItem>
+                        <p className="text-[#737373] text-3xl xl:text-sm font-normal xl:font-bold font-montserrat">
+                          Erkek
+                        </p>
+                        <faPhone
+                          strokeWidth={2.5}
+                          className={`h-3.5 w-3.5 transition-transform ${
+                            openMenu2 ? "rotate-90" : ""
+                          }`}
+                        />
+                      </MenuItem>
+                    </MenuHandler>
+                    <MenuList>
+                      {maleCategories.map((category) => (
+                        <MenuItem
+                          className="text-[#737373] text-3xl xl:text-sm font-normal xl:font-bold font-montserrat"
+                          key={category.id}
+                        >
+                          {category.title}
+                        </MenuItem>
+                      ))}
+                    </MenuList>
+                  </Menu>
+                </MenuList>
+              </Menu>
+            </div>
           </div>
         </div>
         <div className="hidden xl:flex flex-wrap gap-8 text-[#23A6F0] items-center justify-end">
           <div className="flex gap-2">
-            <Link to="/profile">
-              <FontAwesomeIcon icon={faUser} />
-            </Link>
-
-            <Link to="/login">Login</Link>
-            <p>/</p>
-            <Link to="/signup-page">Register</Link>
+            {token ? (
+              <>
+                <p>{user.name}</p>
+                <button type="button" onClick={tokenDelete}>
+                  Exit
+                </button>
+              </>
+            ) : (
+              <>
+                <Link to="/profile">
+                  <FontAwesomeIcon icon={faUser} />
+                </Link>
+                <Link to="/login">Login</Link>
+                <p>/</p>
+                <Link to="/signup-page">Register</Link>
+              </>
+            )}
           </div>
           <Link to="/search">
             <FontAwesomeIcon icon={faMagnifyingGlass} />
