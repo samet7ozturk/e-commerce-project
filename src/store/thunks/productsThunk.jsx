368da4f0a19605235dda5e3c19ce791e2abe96bc
samet7ozturk <sametozturk973@gmail.com>
2023-12-28 21:23:46 +0300

    Infinite-scrool thunk

--- src/store/thunks/productsThunk.jsx
+++ src/store/thunks/productsThunk.jsx
@@ -1,27 +1,37 @@
 import { instanceAxios } from "../../api/api";
+import {
+  fetchMore,
+  setFailed,
+  setFetched,
+  setFetching,
+} from "../actions/productsActions";
 
-export const fetchProducts =
-  ({ category, filter, sort }) =>
-  async (dispatch) => {
-    try {
-      dispatch({ type: "FETCHING" });
+export const fetchProducts = (data) => async (dispatch) => {
+  try {
+    dispatch(setFetching());
 
-      // API'den ürünleri getir
-      const response = await instanceAxios.get("/products", {
-        params: { category, filter, sort },
-      });
+    // API'den ürünleri getir
+    const response = await instanceAxios.get("/products", {
+      params: data,
+    });
 
-      // Başarı durumunda ürünleri al ve store'a gönder
-      dispatch({
-        type: "FETCHED",
-        payload: response.data,
-      });
-      console.log("productsdata :", response.data);
-    } catch (error) {
-      // Hata durumunda hatayı store'a gönder
-      dispatch({
-        type: "FAILED",
-        payload: error.message,
-      });
-    }
-  };
+    // Başarı durumunda ürünleri al ve store'a gönder
+    dispatch(setFetched(response.data));
+    console.log("productsdata :", response.data);
+  } catch (error) {
+    // Hata durumunda hatayı store'a gönder
+    dispatch(setFailed(error.message));
+  }
+};
+
+export const fetchNextPage = (data) => async (dispatch) => {
+  try {
+    const response = await instanceAxios.get("/products", {
+      params: data,
+    });
+    dispatch(fetchMore(response.data));
+    console.log("productsdata :", response.data);
+  } catch (error) {
+    dispatch(setFailed(error.message));
+  }
+};
