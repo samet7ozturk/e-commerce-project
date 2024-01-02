e9e35df8764c86538a96451f2d92c459b574f81a
samet7ozturk <sametozturk973@gmail.com>
2024-01-03 01:16:52 +0300

    Best seller products list

--- src/layout/BestSellerProducts1.jsx
+++ src/layout/BestSellerProducts1.jsx
@@ -1,111 +1,52 @@
 import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { fetchProducts } from "../store/thunks/productsThunk";
 
 import ProductCard from "../components/ProductCard";
 
-import img1 from "../assets/bestseller-products-1.png";
-import img2 from "../assets/bestseller-products-2.png";
-import img3 from "../assets/bestseller-products-3.png";
-import img4 from "../assets/bestseller-products-4.png";
-import img5 from "../assets/bestseller-products-5.png";
-import img6 from "../assets/bestseller-products-6.png";
-import img7 from "../assets/bestseller-products-7.png";
-import img8 from "../assets/bestseller-products-8.png";
+const BestSellerProduct1 = () => {
+  const dispatch = useDispatch();
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [customFilter, setCustomFilter] = useState("");
+  const [selectedSort, setSelectedSort] = useState("");
+  const data = useSelector((state) => state.products.productList);
+  console.log("bestsellerrrrr", data);
 
-const BestSellerProduct = () => {
+  const sortedData = [...data].sort((a, b) => b.rating - a.rating);
+  const topProducts = sortedData.slice(0, 8);
+  console.log("bestsellerrrrr", topProducts);
+
+  useEffect(() => {
+    dispatch(
+      fetchProducts({
+        category: selectedCategory,
+        filter: customFilter,
+        sort: selectedSort,
+      })
+    ).catch((error) => {
+      console.error("Error fetching products:", error);
+    });
+  }, []);
+
   return (
-    <main className="flex flex-col px-[10%]">
-      <div className="flex flex-wrap justify-center gap-x-8 gap-y-16 pt-14 pb-20">
-        <Link
-          to="/product-page"
-          className="hover:scale-110 hover:drop-shadow-xl transition duration-300"
-        >
-          <img
-            src={img1}
-            alt="img1"
-            className="cursor-pointer hover:scale-110 transition duration-300"
-          />
-          <ProductCard />
-        </Link>
-        <Link
-          to="/product-page"
-          className="hover:scale-110 hover:drop-shadow-xl transition duration-300"
-        >
-          <img
-            src={img2}
-            alt="img2"
-            className="cursor-pointer hover:scale-110 transition duration-300"
-          />
-          <ProductCard />
-        </Link>
-        <Link
-          to="/product-page"
-          className="hover:scale-110 hover:drop-shadow-xl transition duration-300"
-        >
-          <img
-            src={img3}
-            alt="img3"
-            className="cursor-pointer hover:scale-110 transition duration-300"
-          />
-          <ProductCard />
-        </Link>
-        <Link
-          to="/product-page"
-          className="hover:scale-110 hover:drop-shadow-xl transition duration-300"
-        >
-          <img
-            src={img4}
-            alt="img4"
-            className="cursor-pointer hover:scale-110 transition duration-300"
-          />
-          <ProductCard />
-        </Link>
-        <Link
-          to="/product-page"
-          className="hover:scale-110 hover:drop-shadow-xl transition duration-300"
-        >
-          <img
-            src={img5}
-            alt="img5"
-            className="cursor-pointer hover:scale-110 transition duration-300"
-          />
-          <ProductCard />
-        </Link>
-        <Link
-          to="/product-page"
-          className="hover:scale-110 hover:drop-shadow-xl transition duration-300"
-        >
-          <img
-            src={img6}
-            alt="img6"
-            className="cursor-pointer hover:scale-110 transition duration-300"
-          />
-          <ProductCard />
-        </Link>
-        <Link
-          to="/product-page"
-          className="hover:scale-110 hover:drop-shadow-xl transition duration-300"
-        >
-          <img
-            src={img7}
-            alt="img7"
-            className="cursor-pointer hover:scale-110 transition duration-300"
-          />
-          <ProductCard />
-        </Link>
-        <Link
-          to="/product-page"
-          className="hover:scale-110 hover:drop-shadow-xl transition duration-300"
-        >
-          <img
-            src={img8}
-            alt="img8"
-            className="cursor-pointer hover:scale-110 transition duration-300"
-          />
-          <ProductCard />
-        </Link>
-      </div>
+    <main className="flex flex-wrap px-[10%] gap-8 justify-center text-center">
+      {topProducts.map((product, index) => (
+        <div className="w-[239px] hover:scale-110 hover:drop-shadow-xl transition duration-300">
+          <Link to="/product-page" key={index}>
+            <img
+              src={product.images[0].url}
+              alt={`img${index + 1}`}
+              className="cursor-pointer hover:scale-110 transition duration-300"
+            />
+          </Link>
+          <p className="pt-4">{product.name}</p>
+          <p className="pt-4">${product.price}</p>
+          <p className="pt-4">{product.rating}</p>
+        </div>
+      ))}
     </main>
   );
 };
 
-export default BestSellerProduct;
+export default BestSellerProduct1;
