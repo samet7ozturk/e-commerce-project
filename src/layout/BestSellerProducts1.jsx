2a87040a6f433eb3058fe3d21898f7e4ebafae43
samet7ozturk <sametozturk973@gmail.com>
2024-01-03 13:24:32 +0300

    Product cart import

--- src/layout/BestSellerProducts1.jsx
+++ src/layout/BestSellerProducts1.jsx
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect, useState } from "react";
 import { fetchProducts } from "../store/thunks/productsThunk";
@@ -7,9 +6,7 @@ import ProductCard from "../components/ProductCard";
 
 const BestSellerProduct1 = () => {
   const dispatch = useDispatch();
-  const [selectedCategory, setSelectedCategory] = useState("");
-  const [customFilter, setCustomFilter] = useState("");
-  const [selectedSort, setSelectedSort] = useState("");
+  const [rating, setrating] = useState("rating:desc");
   const data = useSelector((state) => state.products.productList);
   console.log("bestsellerrrrr", data);
 
@@ -20,9 +17,7 @@ const BestSellerProduct1 = () => {
   useEffect(() => {
     dispatch(
       fetchProducts({
-        category: selectedCategory,
-        filter: customFilter,
-        sort: selectedSort,
+        sort: rating,
       })
     ).catch((error) => {
       console.error("Error fetching products:", error);
@@ -32,18 +27,7 @@ const BestSellerProduct1 = () => {
   return (
     <main className="flex flex-wrap px-[10%] gap-8 justify-center text-center">
       {topProducts.map((product, index) => (
-        <div className="w-[239px] hover:scale-110 hover:drop-shadow-xl transition duration-300">
-          <Link to="/product-page" key={index}>
-            <img
-              src={product.images[0].url}
-              alt={`img${index + 1}`}
-              className="cursor-pointer hover:scale-110 transition duration-300"
-            />
-          </Link>
-          <p className="pt-4">{product.name}</p>
-          <p className="pt-4">${product.price}</p>
-          <p className="pt-4">{product.rating}</p>
-        </div>
+        <ProductCard product={product} key={index} />
       ))}
     </main>
   );
