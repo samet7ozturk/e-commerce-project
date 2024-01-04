bd3dacbd7432ff78f0fda7b23f2b4c85ca688426
samet7ozturk <sametozturk973@gmail.com>
2024-01-04 23:13:52 +0300

    Template

---
+++ src/pages/OrderPage.jsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+const OrderPage = () => {
+  return (
+    <main className=" font-montserrat">
+      <Header />
+      <Footer />
+    </main>
+  );
+};
+
+export default OrderPage;
